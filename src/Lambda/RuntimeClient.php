a3d1ef160d66dcb57cbd11ba5f0e815c87c12677
Evgenii Nasyrov <enasyrov@icloud.com>
2022-12-20 13:18:06 +0000

    Add runtime client

---
+++ src/Lambda/RuntimeClient.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPFortress\Runtime\Lambda;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Throwable;
+use WPFortress\Runtime\Contracts\InvocationContract;
+use WPFortress\Runtime\Contracts\InvocationFactoryContract;
+use WPFortress\Runtime\Contracts\InvocationResponseContract;
+
+final class RuntimeClient
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private InvocationFactoryContract $invocationFactory,
+    ) {
+    }
+
+    public function retrieveNextInvocation(): InvocationContract
+    {
+        return $this->invocationFactory->make(
+            $this->httpClient->request('GET', '/2018-06-01/runtime/invocation/next')
+        );
+    }
+
+    public function sendInvocationResponse(InvocationContract $invocation, InvocationResponseContract $response): void
+    {
+        $this->httpClient->request(
+            'POST',
+            "/2018-06-01/runtime/invocation/{$invocation->getContext()->getAwsRequestId()}/response",
+            ['json' => $response->toApiGatewayFormat()]
+        );
+    }
+
+    public function sendInvocationError(InvocationContract $invocation, Throwable $exception): void
+    {
+        $this->httpClient->request(
+            'POST',
+            "/2018-06-01/runtime/invocation/{$invocation->getContext()->getAwsRequestId()}/error",
+            ['headers' => $this->resolveErrorHeaders($exception), 'json' => $this->resolveErrorPayload($exception)]
+        );
+    }
+
+    public function sendInitialisationError(Throwable $exception): void
+    {
+        $this->httpClient->request(
+            'POST',
+            '/2018-06-01/runtime/init/error',
+            ['headers' => $this->resolveErrorHeaders($exception), 'json' => $this->resolveErrorPayload($exception)]
+        );
+    }
+
+    /** @return array<string, string> */
+    private function resolveErrorHeaders(Throwable $exception): array
+    {
+        return [
+            'Lambda-Runtime-Function-Error-Type' => 'Unhandled',
+        ];
+    }
+
+    /** @return array<string, mixed> */
+    private function resolveErrorPayload(Throwable $error): array
+    {
+        return [
+            'errorType' => get_class($error),
+            'errorMessage' => $error->getMessage(),
+            'stackTrace' => explode(PHP_EOL, $error->getTraceAsString()),
+        ];
+    }
+}
---
+++ tests/Unit/Lambda/RuntimeClientTest.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPFortress\Runtime\Tests\Lambda;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use WPFortress\Runtime\Contracts\InvocationContextContract;
+use WPFortress\Runtime\Contracts\InvocationContract;
+use WPFortress\Runtime\Contracts\InvocationFactoryContract;
+use WPFortress\Runtime\Contracts\InvocationResponseContract;
+use WPFortress\Runtime\Lambda\RuntimeClient;
+
+final class RuntimeClientTest extends TestCase
+{
+    /** @test */
+    public function it_retrieves_next_invocation(): void
+    {
+        $mockedResponse = new MockResponse('', ['http_code' => 200]);
+        $mockedHttpClient = new MockHttpClient($mockedResponse);
+
+        $stubbedInvocation = $this->createStub(InvocationContract::class);
+
+        $mockedInvocationFactory = $this->createMock(InvocationFactoryContract::class);
+        $mockedInvocationFactory
+            ->expects(self::once())
+            ->method('make')
+            ->willReturn($stubbedInvocation);
+
+        $runtimeClient = new RuntimeClient($mockedHttpClient, $mockedInvocationFactory);
+        $invocation = $runtimeClient->retrieveNextInvocation();
+
+        self::assertSame('GET', $mockedResponse->getRequestMethod());
+        self::assertStringEndsWith('/2018-06-01/runtime/invocation/next', $mockedResponse->getRequestUrl());
+        self::assertSame($stubbedInvocation, $invocation);
+    }
+
+    /** @test */
+    public function it_sends_invocation_response(): void
+    {
+        $mockedResponse = new MockResponse('', ['http_code' => 202]);
+        $mockedHttpClient = new MockHttpClient($mockedResponse);
+
+        $mockedInvocationContext = $this->createMock(InvocationContextContract::class);
+        $mockedInvocationContext
+            ->expects(self::once())
+            ->method('getAwsRequestId')
+            ->willReturn('8476a536-e9f4-11e8-9739-2dfe598c3fcd');
+
+        $mockedInvocation = $this->createMock(InvocationContract::class);
+        $mockedInvocation
+            ->expects(self::once())
+            ->method('getContext')
+            ->willReturn($mockedInvocationContext);
+
+        $mockedInvocationResponse = $this->createMock(InvocationResponseContract::class);
+        $mockedInvocationResponse
+            ->expects(self::once())
+            ->method('toApiGatewayFormat')
+            ->willReturn(['foo' => 'bar']);
+
+        $stubbedInvocationFactoryContract = $this->createStub(InvocationFactoryContract::class);
+
+        $runtimeClient = new RuntimeClient($mockedHttpClient, $stubbedInvocationFactoryContract);
+        $runtimeClient->sendInvocationResponse($mockedInvocation, $mockedInvocationResponse);
+
+        self::assertSame('POST', $mockedResponse->getRequestMethod());
+        self::assertStringEndsWith(
+            '/2018-06-01/runtime/invocation/8476a536-e9f4-11e8-9739-2dfe598c3fcd/response',
+            $mockedResponse->getRequestUrl()
+        );
+        self::assertContains('Content-Type: application/json', $mockedResponse->getRequestOptions()['headers']);
+        self::assertSame('{"foo":"bar"}', $mockedResponse->getRequestOptions()['body']);
+    }
+
+    /** @test */
+    public function it_sends_invocation_error(): void
+    {
+        $mockedResponse = new MockResponse('', ['http_code' => 202]);
+        $mockedHttpClient = new MockHttpClient($mockedResponse);
+
+        $mockedInvocationContext = $this->createMock(InvocationContextContract::class);
+        $mockedInvocationContext
+            ->expects(self::once())
+            ->method('getAwsRequestId')
+            ->willReturn('8476a536-e9f4-11e8-9739-2dfe598c3fcd');
+
+        $mockedInvocation = $this->createMock(InvocationContract::class);
+        $mockedInvocation
+            ->expects(self::once())
+            ->method('getContext')
+            ->willReturn($mockedInvocationContext);
+
+        $exception = new Exception('Test error');
+
+        $stubbedInvocationFactoryContract = $this->createStub(InvocationFactoryContract::class);
+
+        $runtimeClient = new RuntimeClient($mockedHttpClient, $stubbedInvocationFactoryContract);
+        $runtimeClient->sendInvocationError($mockedInvocation, $exception);
+
+        self::assertSame('POST', $mockedResponse->getRequestMethod());
+        self::assertStringEndsWith(
+            '/2018-06-01/runtime/invocation/8476a536-e9f4-11e8-9739-2dfe598c3fcd/error',
+            $mockedResponse->getRequestUrl()
+        );
+        self::assertContains(
+            'Lambda-Runtime-Function-Error-Type: Unhandled',
+            $mockedResponse->getRequestOptions()['headers']
+        );
+        self::assertContains('Content-Type: application/json', $mockedResponse->getRequestOptions()['headers']);
+        self::assertStringStartsWith(
+            '{"errorType":"Exception","errorMessage":"Test error","stackTrace":[',
+            $mockedResponse->getRequestOptions()['body']
+        );
+    }
+
+    /** @test */
+    public function it_sends_initialisation_error(): void
+    {
+        $mockedResponse = new MockResponse('', ['http_code' => 202]);
+        $mockedHttpClient = new MockHttpClient($mockedResponse);
+
+        $exception = new Exception('Test error');
+
+        $stubbedInvocationFactoryContract = $this->createStub(InvocationFactoryContract::class);
+
+        $runtimeClient = new RuntimeClient($mockedHttpClient, $stubbedInvocationFactoryContract);
+        $runtimeClient->sendInitialisationError($exception);
+
+        self::assertSame('POST', $mockedResponse->getRequestMethod());
+        self::assertStringEndsWith('/2018-06-01/runtime/init/error', $mockedResponse->getRequestUrl());
+        self::assertContains(
+            'Lambda-Runtime-Function-Error-Type: Unhandled',
+            $mockedResponse->getRequestOptions()['headers']
+        );
+        self::assertContains('Content-Type: application/json', $mockedResponse->getRequestOptions()['headers']);
+        self::assertStringStartsWith(
+            '{"errorType":"Exception","errorMessage":"Test error","stackTrace":[',
+            $mockedResponse->getRequestOptions()['body']
+        );
+    }
+}
