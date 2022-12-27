132542ceed21becee3d8c1e54647c04d36a8a125
Evgenii Nasyrov <enasyrov@icloud.com>
2022-12-27 00:09:07 +0000

    Refactor fastcgi request

--- src/FastCGI/Request.php
+++ src/FastCGI/Request.php
@@ -4,25 +4,142 @@ declare(strict_types=1);
 
 namespace WPFortress\Runtime\FastCGI;
 
-use hollodotme\FastCGI\Requests\AbstractRequest;
+use hollodotme\FastCGI\Interfaces\ProvidesRequestData;
 
-final class Request extends AbstractRequest
+final class Request implements ProvidesRequestData
 {
+    public const DEFAULT_GATEWAY_INTERFACE = 'FastCGI/1.0';
+
+    public const DEFAULT_REQUEST_METHOD = 'GET';
+
+    public const DEFAULT_SERVER_SOFTWARE = 'WPFortress';
+
+    public const DEFAULT_REMOTE_ADDR = '192.168.0.1';
+    public const DEFAULT_REMOTE_PORT = 9985;
+
+    public const DEFAULT_SERVER_ADDR = '127.0.0.1';
+    public const DEFAULT_SERVER_PORT = 80;
+    public const DEFAULT_SERVER_NAME = 'localhost';
+    public const DEFAULT_SERVER_PROTOCOL = 'HTTP/1.1';
+
+    public const DEFAULT_CONTENT_TYPE = 'application/x-www-form-urlencoded';
+    public const DEFAULT_CONTENT_LENGTH = 0;
+
+    /**
+     * @param array{
+     *  GATEWAY_INTERFACE: string,
+     *  REQUEST_METHOD: string,
+     *  REQUEST_URI: string,
+     *  SCRIPT_FILENAME: string,
+     *  SERVER_SOFTWARE: string,
+     *  REMOTE_ADDR: string,
+     *  REMOTE_PORT: int,
+     *  SERVER_ADDR: string,
+     *  SERVER_PORT: int,
+     *  SERVER_NAME: string,
+     *  SERVER_PROTOCOL: string,
+     *  CONTENT_TYPE: string,
+     *  CONTENT_LENGTH: int,
+     * } $parameters
+     */
     public function __construct(
-        private string $method,
-        string $scriptFilename,
-        string $content
+        private string $content,
+        private array $parameters,
     ) {
-        parent::__construct($scriptFilename, $content);
+    }
+
+    public function getGatewayInterface(): string
+    {
+        return $this->parameters['GATEWAY_INTERFACE'] ?? self::DEFAULT_GATEWAY_INTERFACE;
     }
 
     public function getRequestMethod(): string
     {
-        return $this->method;
+        return $this->parameters['REQUEST_METHOD'] ?? self::DEFAULT_REQUEST_METHOD;
+    }
+
+    public function getScriptFilename(): string
+    {
+        return $this->parameters['SCRIPT_FILENAME'] ?? '';
     }
 
     public function getServerSoftware(): string
     {
-        return 'WPFortress';
+        return $this->parameters['SERVER_SOFTWARE'] ?? self::DEFAULT_SERVER_SOFTWARE;
+    }
+
+    public function getRemoteAddress(): string
+    {
+        return $this->parameters['REMOTE_ADDR'] ?? self::DEFAULT_REMOTE_ADDR;
+    }
+
+    public function getRemotePort(): int
+    {
+        return $this->parameters['REMOTE_PORT'] ?? self::DEFAULT_REMOTE_PORT;
+    }
+
+    public function getServerAddress(): string
+    {
+        return $this->parameters['SERVER_ADDR'] ?? self::DEFAULT_SERVER_ADDR;
+    }
+
+    public function getServerPort(): int
+    {
+        return $this->parameters['SERVER_PORT'] ?? self::DEFAULT_SERVER_PORT;
+    }
+
+    public function getServerName(): string
+    {
+        return $this->parameters['SERVER_NAME'] ?? self::DEFAULT_SERVER_NAME;
+    }
+
+    public function getServerProtocol(): string
+    {
+        return $this->parameters['SERVER_PROTOCOL'] ?? self::DEFAULT_SERVER_PROTOCOL;
+    }
+
+    public function getContentType(): string
+    {
+        return $this->parameters['CONTENT_TYPE'] ?? self::DEFAULT_CONTENT_TYPE;
+    }
+
+    public function getContentLength(): int
+    {
+        return $this->parameters['CONTENT_LENGTH'] ?? self::DEFAULT_CONTENT_LENGTH;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getCustomVars(): array
+    {
+        return $this->parameters;
+    }
+
+    public function getParams(): array
+    {
+        return $this->parameters;
+    }
+
+    public function getRequestUri(): string
+    {
+        return $this->parameters['REQUEST_URI'] ?? '';
+    }
+
+    public function getResponseCallbacks(): array
+    {
+        return [];
+    }
+
+    public function getFailureCallbacks(): array
+    {
+        return [];
+    }
+
+    public function getPassThroughCallbacks(): array
+    {
+        return [];
     }
 }
--- tests/Unit/FastCGI/RequestTest.php
+++ tests/Unit/FastCGI/RequestTest.php
@@ -4,23 +4,321 @@ declare(strict_types=1);
 
 namespace WPFortress\Runtime\Tests\FastCGI;
 
-use hollodotme\FastCGI\Requests\AbstractRequest;
+use hollodotme\FastCGI\Interfaces\ProvidesRequestData;
 use PHPUnit\Framework\TestCase;
 use WPFortress\Runtime\FastCGI\Request;
 
 final class RequestTest extends TestCase
 {
     /** @test */
-    public function it_forms_correct_fastcgi_request(): void
+    public function it_implements_provides_request_data_contract(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertInstanceOf(ProvidesRequestData::class, $request);
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_content(): void
     {
         $request = new Request(
-            method: $expectedMethod = 'GET',
-            scriptFilename: '',
-            content: ''
+            content: 'foo',
+            parameters: [],
         );
 
-        self::assertInstanceOf(AbstractRequest::class, $request);
-        self::assertSame($expectedMethod, $request->getRequestMethod());
-        self::assertSame('WPFortress', $request->getServerSoftware());
+        self::assertSame('foo', $request->getContent());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_gateway_interface(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_GATEWAY_INTERFACE, $request->getGatewayInterface());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_gateway_interface(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['GATEWAY_INTERFACE' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getGatewayInterface());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_request_method(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_REQUEST_METHOD, $request->getRequestMethod());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_request_method(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['REQUEST_METHOD' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getRequestMethod());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_script_filename(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame('', $request->getScriptFilename());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_script_filename(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SCRIPT_FILENAME' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getScriptFilename());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_server_software(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_SERVER_SOFTWARE, $request->getServerSoftware());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_server_software(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SERVER_SOFTWARE' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getServerSoftware());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_remote_address(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_REMOTE_ADDR, $request->getRemoteAddress());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_remote_address(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['REMOTE_ADDR' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getRemoteAddress());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_remote_port(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_REMOTE_PORT, $request->getRemotePort());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_remote_port(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['REMOTE_PORT' => 8080],
+        );
+
+        self::assertSame(8080, $request->getRemotePort());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_server_address(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_SERVER_ADDR, $request->getServerAddress());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_server_address(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SERVER_ADDR' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getServerAddress());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_server_port(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_SERVER_PORT, $request->getServerPort());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_server_port(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SERVER_PORT' => 8080],
+        );
+
+        self::assertSame(8080, $request->getServerPort());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_server_name(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_SERVER_NAME, $request->getServerName());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_server_name(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SERVER_NAME' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getServerName());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_server_protocol(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_SERVER_PROTOCOL, $request->getServerProtocol());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_server_protocol(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['SERVER_PROTOCOL' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getServerProtocol());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_content_type(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_CONTENT_TYPE, $request->getContentType());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_content_type(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['CONTENT_TYPE' => 'foo'],
+        );
+
+        self::assertSame('foo', $request->getContentType());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_content_length(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame(Request::DEFAULT_CONTENT_LENGTH, $request->getContentLength());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_content_length(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['CONTENT_LENGTH' => 150],
+        );
+
+        self::assertSame(150, $request->getContentLength());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_variables(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['FOO' => 'bar'],
+        );
+
+        self::assertSame(['FOO' => 'bar'], $request->getCustomVars());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_parameters(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['FOO' => 'bar'],
+        );
+
+        self::assertSame(['FOO' => 'bar'], $request->getParams());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_default_request_uri(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame('', $request->getRequestUri());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_custom_request_uri(): void
+    {
+        $request = new Request(
+            content: '',
+            parameters: ['REQUEST_URI' => '/foo'],
+        );
+
+        self::assertSame('/foo', $request->getRequestUri());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_response_callbacks(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame([], $request->getResponseCallbacks());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_failure_callbacks(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame([], $request->getFailureCallbacks());
+    }
+
+    /** @test */
+    public function it_forms_correct_request_with_pass_through_callbacks(): void
+    {
+        $request = new Request(content: '', parameters: []);
+
+        self::assertSame([], $request->getPassThroughCallbacks());
     }
 }
