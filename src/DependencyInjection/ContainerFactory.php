626ab8097d09b1fa3b0c977816662cbe78ea5940
Evgenii Nasyrov <1330477+nasyrov@users.noreply.github.com>
2023-01-08 00:30:20 +0000

    Encapsulate container building logic into a factory class (#16)

---
+++ src/DependencyInjection/ContainerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPFortress\Runtime\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class ContainerFactory
+{
+    public function makeFromConfig(string $path): ContainerInterface
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        $loader = new YamlFileLoader($containerBuilder, new FileLocator());
+        $loader->load($path);
+
+        $containerBuilder->compile(true);
+
+        return $containerBuilder;
+    }
+}
