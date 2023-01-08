9dd8e58b0a4244f3a4566485ec829ba63858209d
Evgenii Nasyrov <enasyrov@icloud.com>
2023-01-08 21:49:18 +0000

    Refactor process command to use lambda runtime processor service

--- src/Console/Commands/ProcessCommand.php
+++ src/Console/Commands/ProcessCommand.php
@@ -8,17 +8,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Throwable;
-use WPFortress\Runtime\Contracts\FastCGIProcessManagerContract;
-use WPFortress\Runtime\Contracts\InvocationHandlerBusContract;
-use WPFortress\Runtime\Contracts\LambdaRuntimeClientContract;
+use WPFortress\Runtime\Contracts\LambdaRuntimeProcessorContract;
 
 final class ProcessCommand extends Command
 {
     public function __construct(
-        private FastCGIProcessManagerContract $processManager,
-        private LambdaRuntimeClientContract $runtimeClient,
-        private InvocationHandlerBusContract $handlerBus,
+        private LambdaRuntimeProcessorContract $lambdaRuntimeProcessor,
     ) {
         parent::__construct();
     }
@@ -43,35 +38,14 @@ final class ProcessCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if ($input->getOption(name: 'with-fastcgi') === true) {
-            try {
-                $this->processManager->start();
-            } catch (Throwable $exception) {
-                $this->runtimeClient->sendInitialisationError($exception);
-
-                return self::FAILURE;
-            }
+            $this->lambdaRuntimeProcessor->startFastCGIProcess();
         }
 
-        $invocations = 0;
         $maxInvocations = $input->getOption(name: 'max-invocations');
-
-        while (true) {
-            $invocation = $this->runtimeClient->retrieveNextInvocation();
-
-            try {
-                $this->runtimeClient->sendInvocationResponse(
-                    invocation: $invocation,
-                    response: $this->handlerBus->handle(invocation: $invocation)->handle(invocation: $invocation),
-                );
-
-                ++$invocations;
-            } catch (Throwable $exception) {
-                $this->runtimeClient->sendInvocationError($invocation, $exception);
-            }
-
-            if ($invocation >= $maxInvocations) {
-                return self::SUCCESS;
-            }
+        for ($invocations = 0; $invocations < $maxInvocations; $invocations++) {
+            $this->lambdaRuntimeProcessor->processNextInvocation();
         }
+
+        return self::SUCCESS;
     }
 }
