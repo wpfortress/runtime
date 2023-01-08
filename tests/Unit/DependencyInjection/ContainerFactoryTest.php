aac3344c5d9a5b3089ebb27dfd833d264a7061ef
Evgenii Nasyrov <enasyrov@icloud.com>
2023-01-08 00:29:03 +0000

    Encapsulate container building logic into a factory class

---
+++ tests/Unit/DependencyInjection/ContainerFactoryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPFortress\Runtime\Tests\FastCGI;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use WPFortress\Runtime\DependencyInjection\ContainerFactory;
+
+final class ContainerFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_makes_container_from_given_config(): void
+    {
+        $containerFactory = new ContainerFactory();
+        $container = $containerFactory->makeFromConfig(__DIR__ . '/../../../config/services.yaml');
+
+        self::assertInstanceOf(ContainerInterface::class, $container);
+    }
+}
